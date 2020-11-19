c3b8b10e52e33969b191716d13ac060ca6dffa38
Mason M <m8mamiya@gmail.com>
2020-11-19 11:12:16 -0800

    console log command for randomization added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
 var specialChar = ["!", "\"", "#", "$", "%","\&", "\'", "(", ")", "*", "-", "_", "+", "=", ".", "/",  ":", ";", "<", ">", "?", "@", "\\", "[", "]", "^", "`", "~", "{", "}", "|"]
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',]
-var lowerase = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var lowercase = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 // Write password to the #password input
 function writePassword() {
@@ -22,3 +22,16 @@ alert('generate password')
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+const random = Math.floor(Math.random() * numbers.length);
+console.log(random, numbers[random]);
+
+const random = Math.floor(Math.random() * specialChar.length);
+console.log(random, specialChar[random]);
+
+const random = Math.floor(Math.random() * lowercase.length);
+console.log(random, lowercase[random]);
+
+const random = Math.floor(Math.random() * uppercase.length);
+console.log(random, uppercase[random]);
